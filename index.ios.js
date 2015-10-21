50edb19a42f254d3b142bfb7e324252447b6b38e
Ben Teese <ben.teese@gmail.com>
2015-10-21 14:45:19 +1100

    Got a basic text input being displayed

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -10,44 +6,29 @@ var {
   StyleSheet,
   Text,
   View,
+  TextInput
 } = React;
 
-var YOW2015 = React.createClass({
-  render: function() {
+class YOW2015 extends React.Component {
+  constructor() {
+    super();
+    this.state = {txt: 'Hello'};
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TextInput style={styles.textInput}
+        value={this.state.txt}
+        onChangeText={txt => this.setState({txt})}
+      />
     );
   }
-});
+}
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  textInput: {
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('YOW2015', () => YOW2015);
